30c31f958ce5901d696b95b033b0f52386c850dd
Walter-Macelo-Emanuel <wappolonia@gmail.com@users.noreply.github.com>
2020-11-21 21:44:20 -0300

    js aclaraciones

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-"use strict"
+"use strict" //uso strict mode --impide usar palabras reservadas.
 
-// hora
+//Pedir la hora.
 var time = new Date();
 console.log((time.getHours) + ":" +
     time.getHours() + ":" +
     time.getMinutes() + ":" +
     time.getSeconds());
 
-//ejemplo variable global
+//ejemplo variable global.
 
 var nombre = "Walter";
 console.log(nombre)
 
+//ejemplo variable local.
 function saludo() {
     let nombre = "Marcelo";
     console.log(nombre)
